da0b91553b7d4bb84dbc0ba6e7eee5e130f913b0
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-12 12:27:47 +0530

    Update service-catalog-product.tf

--- modules/service-catalog-product/service-catalog-product.tf
+++ modules/service-catalog-product/service-catalog-product.tf
@@ -1,17 +1,17 @@
 resource "aws_servicecatalog_product" "example" {
-  name          = "simples3bucket1"
+  name          = "simples3bucket2"
   owner         = "akshaya"
   description   = "A simple S3 bucket product"
   distributor   = "example-distributor"
-  support_description = "Example support description"
-  support_email       = "support@example.com"
-  support_url         = "https://example.com/support"
-  type          = "CLOUD_FORMATION_TEMPLATE"
+  support_email = "support@example.com"
+  support_url   = "https://example.com/support"
+  type          = "EXTERNAL"
 
   provisioning_artifact_parameters {
-    name           = "v1"
-    description    = "Version 1"
-    template_url   = "https://s3.amazonaws.com/akired18/template.yaml"
-    type           = "CLOUD_FORMATION_TEMPLATE"
+    name                         = "v1"
+    description                  = "Version 1"
+    type                         = "EXTERNAL"  # Ensure this is set to EXTERNAL
+    template_url                 = var.products[count.index].template_url
+    disable_template_validation  = true
   }
 }
