73b565f0f5a736006a4655582c9d735f71b83e4d
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-12 12:22:43 +0530

    Delete modules/service-catalog-portfolio/service-catalog-portfolio.tf

--- modules/service-catalog-portfolio/service-catalog-portfolio.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_servicecatalog_portfolio" "s3_bucket_portfolio" {
-  name          = "s3bucket1"
-  description   = "Portfolio to manage S3 bucket products"
-  provider_name = "akshaya"
-}
-
-output "portfolio_id" {
-  value = aws_servicecatalog_portfolio.s3_bucket_portfolio.id
-}
